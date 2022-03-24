50a0fbb984d68268e5bc01eeb5c9123862a0afde
POIRON Lea <lea.poiron@telecomnancy.eu>
2022-03-24 21:34:52 +0100

    Début de structure web

---
+++ site/app.py
@@ -0,0 +1,33 @@
+import re
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+import sqlite3
+
+
+app=Flask(__name__)
+
+
+def testconnect():
+    global login
+    if 'login' in globals() :
+        return True
+    else:
+        return False
+
+@app.route('/')
+def initial():
+    return render_template('prelayout.html')
+
+@app.route('/inscription',methods=["GET","POST"])
+def inscription():
+    return render_template('inscription.html')
+
+@app.route('/login')
+def connexion():
+    return render_template('login.html')
+
+@app.route('/jeusanslogin')
+def jeusanslogin():
+    return render_template('jeusanslogin.html')
