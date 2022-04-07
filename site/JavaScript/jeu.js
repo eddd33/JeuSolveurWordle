657ef9e5546f6ae348e020ff7c686fb546ae687e
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2022-04-07 12:35:26 +0200

    récupération des paramètres longueur du mot et nombre de chances pour créer le cadre

--- site/JavaScript/jeu.js
+++ site/JavaScript/jeu.js
@@ -1,11 +1,16 @@
+var longueur = document.getElementById(long).value;
+
+var tentatives = document.getElementById(tryNB).value;
+
+
 function initJeu() {
     let tableau = document.getElementById("wordle");
 
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < tentatives; i++) {
         let rang = document.createElement("div")
         rang.className = "rang-lettre"
         
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < longueur; j++) {
             let box = document.createElement("div")
             box.className = "case-lettre"
             rang.appendChild(box)
