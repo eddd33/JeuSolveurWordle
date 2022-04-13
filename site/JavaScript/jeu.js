e28c6e9018216a8cadc4311ad12b7a2ce9f0da51
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2022-04-13 17:42:17 +0200

    remplissage d une ligne au clavier

--- site/JavaScript/jeu.js
+++ site/JavaScript/jeu.js
@@ -9,11 +9,12 @@ function initJeu() {
     for (let i = 0; i < 5; i++) {
         let rang = document.createElement("div")
         rang.className = "rang"
+        rang.id = "ligne-" + i
         
         for (let j = 0; j < 5; j++) {
             let box = document.createElement("span")
             box.className = "case"
-            box.id = "key"
+            box.id = "ligne-" + i + "-key-" + j
             rang.appendChild(box)
             box.textContent = '.';
         }
@@ -24,45 +25,55 @@ function initJeu() {
 
 initJeu()
 
+let lettreAct = 0;
 
-//let lettreSuiv = 0;
+let rangAct = 0;
 
 document.addEventListener("keyup",(e) => {
 
 
     let key = document.getElementById("key");
-    key.textContent = e.key;
-    console.log(e.key);
+    //key.textContent = e.key;
+    //console.log(e.key);
 
     if (tryNB === 0){
         return
     }
     
+    let pressKey = String(e.key);
 
-    if (key === "Backspace" && lettreSuiv !== 0){
+    console.log(pressKey);
+
+    if (pressKey === "Backspace" && lettreSuiv !== 0){
         supprLettre()
         return
     }
 
-    //let guess = key.match(/[A-Z]/)
-    //if (!guess){
-      //  return
-    //} else{
-      //  insertLettre(key)
-    //}
+    let guess = pressKey.match(/[A-Z]/gi)
+    if (!guess || guess.length > 1){
+        return
+    } else{
+        insertLettre(pressKey)
+    }
 
 })
 
 function insertLettre(key){
-    if (lettreSuiv == longueur){
-        return
+    if (lettreAct < 5 && rangAct < 5 ){
+        const boite = document.getElementById('ligne-' + rangAct + '-key-' + lettreAct)
+        boite.textContent = key
+
+        boite.setAttribute('data', key)
+        lettreAct++
     }
 
-    let rang = document.getElementsByClassName("rang")[6 - tryNB];
-    let box = rang.children[lettreSuiv - 1];
-    box.textContent = key
-    box.classList.add("box-pleine")
-    lettreSuiv = lettreSuiv +1
+    //key = key.toLowerCase()
+
+    //let rang = document.getElementById("ligne")[5 - 5];
+    //let box = rang.children[lettreSuiv];
+    //box.textContent = key
+    //box.classList.add("box-pleine")
+    //lettreSuiv = lettreSuiv +1
 }
 
 function supprLettre(){
