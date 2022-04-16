b9306e96b3035219a7bc55f21a7dcaec460325bd
Laurent NoeLaurent <noe-laurent.laurent@telecomnancy.eu>
2022-04-16 18:47:26 +0200

    Solveur fonctionnel, a améliorer

--- solveur/solveurlol.py
+++ solveur/solveurlol.py
@@ -18,6 +18,9 @@ mal=[]
 
 #Propose un mot
 def prop(motslongueur,bonnes,mal,fausses,dejapropose):
+    malponctuel=[]
+    bonnesponctuel=[]
+    faussesponctuel=[]
     proposition=trouveurmot(motslongueur,dejapropose)
     dejapropose.append(proposition)
     print(proposition)
@@ -32,13 +35,16 @@ def prop(motslongueur,bonnes,mal,fausses,dejapropose):
     for i in range(len(demandelist)):               #on ajoute les lettres et les emplacements au bons endroit
         if demandelist[i]=='2':
             bonnes.append((proposition[i],i))
+            bonnesponctuel.append((proposition[i],i))
         elif demandelist[i]=='1':
             mal.append((proposition[i],i))
+            malponctuel.append((proposition[i],i))
         else:
             fausses.append(proposition[i])
+            faussesponctuel.append(proposition[i])
 
-    motslongueur=transformation(motslongueur,bonnes,mal,fausses)
-    print(motslongueur)
+    motslongueur=transformation(motslongueur,bonnes,mal,fausses,bonnesponctuel,malponctuel,faussesponctuel)
+    #print(motslongueur)
     prop(motslongueur,bonnes,mal,fausses,dejapropose)
 
 prop(motslongueur,bonnes,mal,fausses,dejapropose)
--- solveur/transformateur.py
+++ solveur/transformateur.py
@@ -1,5 +1,9 @@
-def transformation(L,bonnes,mal,fausses):
+def transformation(L,bonnes,mal,fausses,bonnesponctuel,malponctuel,faussesponctuel):
     T=[]
+    bonnes=singularite(bonnes)
+    mal=singularite(mal)
+    fausses=singularite(fausses)
+
     #si les lettres bonnes sont au bonne endroit
 
     if len(bonnes)!=0:
@@ -29,33 +33,103 @@ def transformation(L,bonnes,mal,fausses):
                 T.remove(mot)
                 break
 
-    ubuf=[]                                         #liste contenant les lettre qui sont dans bonnes (ou mal) et aussi fausses
-    for f in fausses:
-        for b in bonnes:
-            if f==b[0] and not f in ubuf:
-                ubuf.append(f)
-        for m in mal:
-            if f==m[0] and not f in ubuf:
-                ubuf.append(f)
+    # ubuf=[]                                         #liste contenant les lettres qui sont dans bonnes (ou mal) et aussi fausses
+    # for f in fausses:
+    #     for b in bonnes:
+    #         if f==b[0] and not f in ubuf:
+    #             ubuf.append(f)
+    #     fsingularite(L)or m in mal:
+    #         if f==m[0] and not f in ubuf:
+    #             ubuf.append(f)
 
-                                         #compt de lettres bonnes ou mal placees  où une occurence de cette lettre est fausse
-    for u in ubuf:
-        compt=0
+    #                                      #compt de lettres bonnes ou mal placees  où une occurence de cette lettre est fausse
+    # for u in ubuf:
+    #     compt=0
+    #     for b in bonnes:
+    #         if b[0]==u:
+    #             compt+=1
+    #     for m in malponctuel:
+    #         if m[0]==u:
+    #             compt+=1
+    #     print(compt)  
+    #     for mot in T:
+    #         c=0
+    #         for lettre in mot:
+    #             if lettre == u:
+    #                 c+=1
+    #         if c>compt:
+    #             T.remove(mot)
+    #             break
+    # print(ubuf)
+
+
+
+
+    aenlever=[]
+    
+    for f in fausses:                   #les deux prochaines boucles for sont pour enlever les fausses lettres (les vraies)
+        #print("f",f)
+        k=0
+        a=0
         for b in bonnes:
-            if b[0]==u:
-                compt+=1
+            if f==b[0]:
+                k+=1
+        if k==0 and f not in aenlever:
+            a+=1
+        k=0
         for m in mal:
-            if m[0]==u:
-                compt+=1
-            
-        for mot in T:
-            c=0
-            for lettre in mot:
-                if lettre == u:
-                    c+=1
-            if c>compt:
+            if f==m[0]:
+                k+=1
+        if k==0 and f not in aenlever:
+            a+=1
+        #print("a",a)
+        if a==2:
+            aenlever.append(f)
+    
+    for mot in T:
+        for lettre in mot:
+            if lettre in aenlever:
                 T.remove(mot)
                 break
+
+    
+
+
+    for f in faussesponctuel:       #Probleme ici
+        c=0
+        for b in bonnesponctuel:  #compte le nombre d'occurence de la fausse lettre dans les lettres bonnes du mot proposé
+            if b[0]==f:
+                c+=1
+        for m in malponctuel:     #compte le nombre d'occurence de la fausse lettre dans les lettres bonnes du mot proposé
+            if m[0]==f:
+                c+=1
+        
+        for mot in T:
+            if occurence(mot,f)>c:
+                print("ca enleve des trucs")
+                T.remove(mot)
+                break
+
+    print(bonnes,mal,malponctuel,fausses)
                     
 
     return T
+
+def singularite(L):
+    a=0
+    for i in range(len(L)):
+        if L[i] in L[:i] or L[i] in L[i+1:]:
+            del L[i]
+            a=1
+            break
+    if a==0:
+        return L
+    else:
+        return singularite(L)
+
+def occurence(mot,lettre):
+    c=0
+    for l in mot:
+        if l==lettre:
+            c+=1
+    return c
