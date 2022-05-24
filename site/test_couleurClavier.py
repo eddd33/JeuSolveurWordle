de8b705e1d5ee3e2c3c5703dc6f99932c3ae3282
PERONNIN Edgar <edgar.peronnin@telecomnancy.eu>
2022-05-24 22:18:36 +0200

    fin des tests

--- site/test_couleurClavier.py
+++ site/test_couleurClavier.py
@@ -1,21 +1,33 @@
 import sutom as s
 
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 def test_couleurClavier_1():
-    res = s.couleurClavier("deux","deux",4,['-','-','-','-'])
-    assert res == (0,0,0,0)
+    res = s.couleurClavier(alphabet,["w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"]
+    ,['-','-','-','-','-'],['-','-','-','-','-'],['-','-','-','-','-'],"deux")
+    assert res == ["w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"]
     
 def test_couleurClavier_2():
-    res = s.couleurClavier("deux","bleu",4,['-','-','-','-'])
-    assert res == (['-','-','-','-'],['-','-','-','-'],['-','e','u','-'],['d','-','-','x'])
+    res = s.couleurClavier(alphabet,["w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"]
+    ,['-','-','-','-'],['-','e','u','-'],['d','-','-','x'],"deux")
+    assert res == ["w","w","w","g","o","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","o","w","w","g","w","w"]
 
 def test_couleurClavier_3():
-    res = s.couleurClavier("appareils","protocole",9,['-','-','-','-','-','-','-','-','-'])
-    assert res == (['-','-','-','-','-','-','-','l','-'],['-','-','-','-','-','-','-','l','-'],['-','p','-','-','r','e','-','-','-'],['a','-','p','a','-','-','i','-','s'])
+    res = s.couleurClavier(alphabet,["w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"]
+    ,['-','-','-','-','-','-','-','l','-'],['-','p','-','-','r','e','-','l','-'],['a','-','p','a','-','-','e','-','s'],"appareils")
+    assert res == ['g','w','w','w','o','w','w','w','w','w','w','v','w','w','w','o','w','o','g','w','w','w','w','w','w','w']
 
 def test_couleurClavier_4():
-    res = s.couleurClavier("plage","minou",5,['-','-','-','-','-'])
-    assert res== (['-','-','-','-','-'],['-','-','-','-','-'],['-','-','-','-','-'],['p','l','a','g','e'])
+    res = s.couleurClavier(alphabet,["w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"]
+    ,['i','-','-','-','-','-','-'],['-','-','-','-','-','-','g'],['-','c','e','b','e','r','-'],"iceberg")
+    assert res== ['w','g','g','w','g','w','o','w','v','w','w','w','w','w','w','w','w','g','w','w','w','w','w','w','w','w']
     
 def test_couleurClavier_5():
-    res = s.couleurClavier("plage","minou",5,['m','i','-','o','-'])
-    assert res== (['m','i','-','o','-'],['-','-','-','-','-'],['-','-','-','-','-'],['p','l','a','g','e'])
+    res = s.couleurClavier(alphabet,['w','g','g','w','g','w','o','w','v','w','w','w','w','w','w','w','w','g','w','w','w','w','w','w','w','w']
+    ,['i','n','-','-','-','-','-'],['-','-','a','-','-','-','g'],['-','-','-','c','t','i','f'],"inactif")
+    assert res== ['o','g','g','w','g','g','o','w','v','w','w','w','w','v','w','w','w','g','w','g','w','w','w','w','w','w']
+
+def test_couleurClavier_6():
+    res = s.couleurClavier(alphabet,['w','g','g','w','g','w','o','w','v','w','w','w','w','w','w','w','w','g','w','w','w','w','w','w','w','w']
+    ,['i','n','e','g','a','u','x'],['-','-','-','-','-','-','-'],['-','-','-','-','-','-','-'],"inegaux")
+    assert res== ['v','g','g','w','v','w','v','w','v','w','w','w','w','v','w','w','w','g','w','w','v','w','w','v','w','w']
