f1c78a861be6e02003069f6710fb385f0e8b0989
Anna <anna.galkowski@telecomnancy.eu>
2022-06-07 18:32:16 +0200

    suite tests

--- Csolveur/solveur_test.c
+++ Csolveur/solveur_test.c
@@ -1,12 +1,11 @@
 #include "solveur.h"
 
 int main(){
-    //assert(recupnb()==5);
     assert(nb_letters("plage")== 5);
     assert(nb_letters("")==0);
 
     assert(occurrences("plage",'o')==0);
-    assert(occurences("monter",'o')==1);
+    assert(occurrences("monter",'o')==1);
     assert(occurrences("fauteuil",'u')==2);
 
     assert(in('a',"plage"));
@@ -28,15 +27,30 @@ int main(){
 
     assert(indexletter("abcdefghijklmnopqrstuvwxyz",'e') == 4);
 
-    char* test2=removeletterinalphabet("abcdefghijklmnopqrstuvwxyz",5);
-    printf("%s\n",test2);
-    assert(strcmp(test2,"abcdeghijklmnopqrstuvwxyz")==0);
-    free(test2);
+    char* test4=removeletterinalphabet("abcdefghijklmnopqrstuvwxyz",5);
+    printf("%s\n",test4);
+    assert(strcmp(test4,"abcdeghijklmnopqrstuvwxyz")==0);
+    free(test4);
+
+    // pour la suite recupnb() = 5
 
     list_t* dico = create_dico();
     assert(!isEmpty(dico));
-    printf("length : %i\n",length(dico));
-    
+    printf("length : %i\n",length(dico)); // on a length : 7087
+
+    assert(length(dico)==7087);
+
+    char* mot = "test";
+    ajout_dico(mot,dico);
+    printf("length : %i\n",length(dico)); // on a bien length : 7088
+    assert(length(dico)==7088);
+    retire(mot, dico);
+    free(mot);
+
+    //char* word = savedwords(5);
+    //assert(strcmp(wordfinder(dico,0),word)==0);
+    //free(word);
+
     /* printf("4ème élement : %s\n",list_get(dico,3));
     assert(strcmp(dico->head->next->ch1,"abats")==0);
     printf("Indice du mot abces : %i\n",list_index_of(dico,"abces")),
@@ -55,7 +69,7 @@ int main(){
 
     //fonctionne seulement avec 5 lettres là
 
-    char* wordtogive=wordfinder(dico,0);    //aires
+    /*char* wordtogive=wordfinder(dico,0);    //aires
 
     printf("début test réduction du dico\n");
     //dico_print(dico);
@@ -71,7 +85,7 @@ int main(){
     printf("--------------------------------------\n");
     wordtogive=wordfinder(dico,3);  //glane
     dico=reduction_dico(wordtogive,"12202",dico);
-    printf("--------------------------------------\n");
+    printf("--------------------------------------\n");*/
     
 
 
